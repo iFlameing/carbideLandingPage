31ac91e5fe70c3f2bb28780781e3f9d6c8c0399d
batrapunit1999@gmail.com <batrapunit1999@gmail.com>
2019-09-02 01:49:43 +0530

    changes

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -14,11 +14,11 @@ class About extends Component {
 Let’s start revolution.</h4>
       <Grid container spacing={3}>
         
-        <Grid item xs={6}>
+        <Grid item xs={12} lg={6} md={6}>
         <div className='image'>
           <img src={fly} alt="about" width='70%' height='60%'></img></div>
         </Grid>
-        <Grid item xs={5}>
+        <Grid item xs={12} lg={6} md={6}>
           <br/>
           <br/>
           <Paper elevation={4}><h2 className='heading1'>ABOUT THE COMPANY</h2></Paper>
@@ -33,7 +33,7 @@ Consumption More Environment Friendly.
     </div>
     <div>
       <div className='mission'>
-      <Grid item xs={4}>
+      <Grid item xs={6} lg={4} xl={3}>
       <Paper elevation={4}><h2 className='mission1'>MISSION</h2></Paper></Grid> </div>
      <div className='points'> <i className="fa fa-hand-o-right">   Develop innovative products (like CARBADEX) which decrease fuel and energy
 consumption.</i>
@@ -47,7 +47,7 @@ energizing. </i>
   
     </div>
     <div className='vision'>
-      <Grid item xs={4}>
+      <Grid item xs={6} lg={4} xl={3}>
       <Paper elevation={4}><h2 className='vision1'>VISION</h2></Paper></Grid> </div>
       <p className='vision2'>A Company That Is Focused On Reducing Fuel Consumption Without
 Compromising The Performance Of The Engine As Well As Making The
@@ -57,13 +57,13 @@ Environment Friendly.
       
       </div><br/>
       <div className='product'>
-      <Grid item xs={4}>
-      <Paper elevation={4}><h2 className='product1'>PRODUCT DETAILS</h2></Paper></Grid> </div>
+      <Grid item xs={6} lg={4} xl={3}>
+      <Paper elevation={4} ><h2 className='product1'>PRODUCT DETAILS</h2></Paper></Grid> </div>
       <p className='product2'>
             The Present Inventional Device is a Milestone in the Direction Towards increase in
 Fuel Efficiency. There Are Many Techniques and ideas available to do so. However By
 Using The Proposed Device, It is Easy to Increase the mileage and power of Any
-Gasoline Or Petrol Internal Combustion Engine. The Present Invention is a Milestone
+Gasoline Or Petrol Internal Combustion Engine. Tshe Present Invention is a Milestone
 in the Field of Automobile Sector to Reduce The Effect of Greenhouse Gases and
 Pollution Level.</p>
       
