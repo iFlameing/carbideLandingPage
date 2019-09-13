e933a1f5edbaac82448e18a11be230741dc2d5dd
batrapunit1999@gmail.com <batrapunit1999@gmail.com>
2019-09-14 00:46:00 +0530

    image

--- src/Component/Achievements/Achievements.js
+++ src/Component/Achievements/Achievements.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import imgachieve from "../../imgachieve.jpg";
 import './Achievements.css';
 class Achievements extends Component{
     render(){
@@ -9,7 +10,10 @@ class Achievements extends Component{
             <div className='achieve1'>
       <Grid item xs={6} lg={4} xl={3}>
       <Paper elevation={4}><h2 className='mission1'>AWARDS AND RECOGNITION</h2></Paper></Grid> </div>
-     <div className='points'> <i className="fa fa-hand-o-right">  DIPP RECOGNIGED STARTUP (REGISTRATION NO. DIPP4505).</i>
+     
+  <Grid container>
+          <Grid item xs={12} lg={6} md={6}>
+          <div id='myDIV'> <i className="fa fa-hand-o-right">  DIPP RECOGNIGED STARTUP (REGISTRATION NO. DIPP4505).</i>
 <i className="fa fa-hand-o-right">  RECOGNISED AS A STARTUP UNDER JAMMU and KASHMIR POLICY
 ON 1ST MARCH 2019 (RECOGNITION NO. 1324914395). </i>
 <i className="fa fa-hand-o-right">  SMVDU-TBIC RECOGNISED STARTUP ON 13TH JUNE 2017. </i>
@@ -17,6 +21,14 @@ ON 1ST MARCH 2019 (RECOGNITION NO. 1324914395). </i>
 <i className="fa fa-hand-o-right">  2ND POSITION BEGGED IN PRODUCT STAGE CATEGORY AT UP STARTUP CONCLAVE-2018, IIM LUCKNOW. </i>
 <i className="fa fa-hand-o-right">  1ST POSITION AND 50 LACS INVESTMENT AGAINST 10% EQUITY SHARE (NEGOTIABLE) BEGGED AT STARTUP JALSA 4TH EDITION-2018 AT DHARMSHALA.</i>
   </div>
+            
+          </Grid>
+          <Grid item xs={12} lg={6} md={6}>
+            <div className="image">
+              <img src={imgachieve} alt="about" width="80%" height="90%"></img>
+            </div>
+          </Grid>
+        </Grid>
   </div>
         );
     }
