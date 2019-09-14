0209afccfa7cd62009330d3bde77b03038871810
batrapunit1999@gmail.com <batrapunit1999@gmail.com>
2019-09-14 19:01:58 +0530

    minor changes

--- src/Component/Achievements/Achievements.js
+++ src/Component/Achievements/Achievements.js
@@ -9,26 +9,18 @@ class Achievements extends Component{
             <div className="achieve">
             <div className='achieve1'>
       <Grid item xs={6} lg={4} xl={3}>
-      <Paper elevation={4}><h2 className='mission1'>AWARDS AND RECOGNITION</h2></Paper></Grid> </div>
+      <Paper elevation={4}><h2 className='missiona1'>AWARDS AND RECOGNITION</h2></Paper></Grid> </div>
      
-  <Grid container>
-          <Grid item xs={12} lg={6} md={6}>
-          <div id='myDIV'> <i className="fa fa-hand-o-right">  DIPP RECOGNIGED STARTUP (REGISTRATION NO. DIPP4505).</i>
-<i className="fa fa-hand-o-right">  RECOGNISED AS A STARTUP UNDER JAMMU and KASHMIR POLICY
-ON 1ST MARCH 2019 (RECOGNITION NO. 1324914395). </i>
-<i className="fa fa-hand-o-right">  SMVDU-TBIC RECOGNISED STARTUP ON 13TH JUNE 2017. </i>
-<i className="fa fa-hand-o-right">  3RD POSITION BEGGED IN GREEN ENERGY, STARTUP CATEGORY AT RISC 2018 AT NIRDPR, HYDERABAD.  </i>
-<i className="fa fa-hand-o-right">  2ND POSITION BEGGED IN PRODUCT STAGE CATEGORY AT UP STARTUP CONCLAVE-2018, IIM LUCKNOW. </i>
-<i className="fa fa-hand-o-right">  1ST POSITION AND 50 LACS INVESTMENT AGAINST 10% EQUITY SHARE (NEGOTIABLE) BEGGED AT STARTUP JALSA 4TH EDITION-2018 AT DHARMSHALA.</i>
-  </div>
-            
-          </Grid>
-          <Grid item xs={12} lg={6} md={6}>
-            <div className="image">
-              <img src={imgachieve} alt="about" width="80%" height="90%"></img>
-            </div>
-          </Grid>
-        </Grid>
+  
+          <Paper elevation={4}>   <div id='myDIV'>  DIPP RECOGNIGED STARTUP (REGISTRATION NO. DIPP4505).<br /><br/>
+  RECOGNISED AS A STARTUP UNDER JAMMU and KASHMIR POLICY
+ON 1ST MARCH 2019 (RECOGNITION NO. 1324914395). <br /><br/>
+  SMVDU-TBIC RECOGNISED STARTUP ON 13TH JUNE 2017. <br /><br/>
+  3RD POSITION BEGGED IN GREEN ENERGY, STARTUP CATEGORY AT RISC 2018 AT NIRDPR, HYDERABAD.  <br/><br/>
+  2ND POSITION BEGGED IN PRODUCT STAGE CATEGORY AT UP STARTUP CONCLAVE-2018, IIM LUCKNOW. <br/><br/>
+ 1ST POSITION AND 50 LACS INVESTMENT AGAINST 10% EQUITY SHARE (NEGOTIABLE) BEGGED AT STARTUP JALSA 4TH EDITION-2018 AT DHARMSHALA.<br/><br/>
+  </div></Paper>
+        
   </div>
         );
     }
